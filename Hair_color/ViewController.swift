967c8a0538b777c1cdeacb6ab2f05c97c36cc972
ziyu <4a7g0119@stust.edu.tw>
2022-03-23 23:01:40 +0800

    Initial Commit

---
+++ Hair_color/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hair_color
+//
+//  Created by 黃子瑜 on 2022/3/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
